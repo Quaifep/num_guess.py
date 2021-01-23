bb3997d3f14be3754d2a8a766f68d795bda1a70d
Paul <quaifep@yahoo.com>
2021-01-22 20:55:05 -0800

    Final?

--- num_guess.py
+++ num_guess.py
@@ -2,13 +2,25 @@
 # Date: 1/20/2021
 # Description: Plays higher or lower guessing game with number entered.
 
-print("Enter the integer for the player to guess.")
+print("Enter the integer for the player to guess. ")
 ans = int(input())
+guesses_taken = 0
 print("Enter your guess")
 guess = int(input())
-if guess < ans:
-    print("Too low - try again:")
-elif guess > ans:
-    print("Too high - try again:")
-elif guess == ans:
-    print("You guessed it in ", guess, "tries.")
+for i in range(guesses_taken, guesses_taken + 1):
+    guesses_taken = 0
+    if guess == ans:
+        print("You guessed it in", guesses_taken + 1, "try.")
+        break
+    while True:
+        guesses_taken = guesses_taken + 1
+        if guess < ans:
+            print("Too low - try again:")
+            guess = int(input())
+        if guess > ans:
+            print("Too high - try again:")
+            guess = int(input())
+        elif guess == ans:
+            guesses_taken = int(guesses_taken)
+            print("You guessed it in", guesses_taken, "tries.")
+            break
